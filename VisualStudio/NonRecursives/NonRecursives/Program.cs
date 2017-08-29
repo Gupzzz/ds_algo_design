c9a1cec85c4fb3881e352dd378de05871bf8e0c5
Arun Guptha <arungupthajrk@gmail.com>
2017-08-29 06:31:35 +0530

    Added solution for calculating max difference with a given set

---
+++ VisualStudio/NonRecursives/NonRecursives/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonRecursives
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            //prog.CalculateMaxProfit();
+            prog.CalculateMaxDifference();
+
+            Console.ReadKey();
+        }
+
+        public void CalculateMaxProfit()
+        {
+            StockPrize stockPrize = new StockPrize();
+
+            int[] shareValue = {100, 20, 50, 40, 60};
+            int maxProfit = stockPrize.CalculateMaxProfit(shareValue);
+
+            Console.WriteLine("The Max profit that can be obtained in the given period for the stock is {0} ", maxProfit);
+        }
+
+        public void CalculateMaxDifference()
+        {
+            MaxDifference maxDiffrenece = new MaxDifference();
+            int[] values = { 100, 20, 50, 40, 60 };
+            int result = maxDiffrenece.CalculateMaxDifference(values);
+            Console.WriteLine("The max difference within the given set of values is {0}", result);
+        }
+    }
+}
---
+++ VisualStudio/NonRecursives/NonRecursives/StockPrize.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonRecursives
+{
+    public class StockPrize
+    {
+        public int CalculateMaxProfit(int[] shareValue)
+        {
+            return 0;
+        }
+    }
+}
