c811daf6db53a2340553bd9ff19eaec497b618d0
Arun Guptha <arungupthajrk@gmail.com>
2018-07-12 12:11:26 +0530

    initial setup

---
+++ VScode/NonRecursives/src/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonRecursives
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+
+            //prog.CalculateMaxProfit();
+
+            prog.CalculateMaxDifference();
+
+            //prog.CalculateMinDiceThrowsSnakeAndLadders();
+
+            Console.ReadKey();
+        }
+
+        public void CalculateMaxProfit()
+        {
+            StockPrize stockPrize = new StockPrize();
+
+            int[] shareValue = {100, 20, 50, 40, 60};
+            stockPrize.CalculateMaxProfit(shareValue, shareValue.Length - 1);
+        }
+
+        public void CalculateMaxDifference()
+        {
+            MaxDifference maxDiffrenece = new MaxDifference();
+            int[] values = { 100, 20, 50, 40, 90, 5 };
+            int result = maxDiffrenece.CalculateMaxDifference(values);
+            Console.WriteLine("The max difference within the given set of values is {0}", result);
+        }
+
+        public void CalculateMinDiceThrowsSnakeAndLadders()
+        {
+            
+            // Let us construct the board given in above diagram
+            int n = 30;
+            int[] moves = new int[n];
+            for (int i = 0; i < n; i++)
+                moves[i] = -1;
+
+            // Ladders
+            moves[2] = 21;
+            moves[4] = 7;
+            moves[10] = 25;
+            moves[19] = 28;
+
+            // Snakes
+            moves[26] = 0;
+            moves[20] = 8;
+            moves[16] = 3;
+            moves[18] = 6;
+
+            SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
+
+            Console.WriteLine(snakeAndLadder.GetMinDiceThrows(moves, n));            
+        }
+    }
+}
---
+++ VScode/NonRecursives/src/StockPrize.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonRecursives
+{
+    public class StockPrize
+    {
+        public void  CalculateMaxProfit(int[] shareValue, int n)
+        {
+            if (n == 1)
+            {
+                return;
+            }
+            int count = 0;
+            List<Interval> solution = new List<Interval>();
+
+            int i = 0;
+            while (i < n)
+            {
+                // Find Local Minima. Note that the limit is (n-2) as we are
+                // comparing present element to the next element. 
+                while ((i < n) && (shareValue[i + 1] <= shareValue[i]))
+                {
+                    i++;
+                }
+
+                // If we reached the end, break as no further solution possible
+                if (i == n)
+                {
+                    break;
+                }
+
+                // Store the index of minima
+                Interval e = new Interval()
+                {
+                    buy = i++
+                };
+                
+                
+                // Find Local Maxima.  Note that the limit is (n-1) as we are
+                // comparing to previous element
+                while ((i <= n) && (shareValue[i] >= shareValue[i - 1]))
+                {
+                    i++;
+                }
+
+                // Store the index of maxima
+                e.sell = i - 1;
+                solution.Add(e);
+
+                // Increment number of buy/sell
+                count++;
+            }
+
+            if (count == 0)
+                Console.WriteLine("There is no day when buying the stock will make profit");
+            else
+                for (int j = 0; j < count; j++)
+                    Console.WriteLine("Buy on day: " + (solution[j].buy + 1) + " Sell on day : " + (solution[j].sell + 1));
+            return;
+        }
+
+    }
+    public class Interval
+    {
+        public int buy, sell;
+
+    }
+}
