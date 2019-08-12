690c353020da6e0ca38b37d65cc1295d76018fd0
Arun Guptha <arunguptha@Three-Cheese-Pizza.redmond.corp.microsoft.com>
2019-08-12 06:53:05 +0530

    August 12th normalizing

---
+++ C++/ds_algo/xcode_project_c++/xcode_project_c++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  xcode_project_c++
+//
+//  Created by Arun Guptha on 16/06/19.
+//  Copyright © 2019 Microsoft. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
