7737a45c623300f466c335c662facc21dac8d720
Arun Guptha JRK <arguptha@microsoft.com>
2020-02-14 13:58:20 +0530

    Cleaning up

---
+++ ds_algo/C#/algoexpert/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace algoexpert
+{
+    public partial class Program
+    {
+        static void Main(string[] args)
+        {
+            #region easy questions
+
+            _TwoNumberSum();
+
+            #endregion
+        }
+    }
+}
