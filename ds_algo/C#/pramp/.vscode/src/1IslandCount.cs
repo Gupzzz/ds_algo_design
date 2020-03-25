0aa96c702031f36ba3c746b90b77cc870f9bdd5e
Arun Guptha <arungupthajrk@gmail.com>
2020-03-25 12:30:15 +0530

    new project for pramp

---
+++ ds_algo/C#/pramp/.vscode/src/1IslandCount.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace pramp
+{
+    public partial class Program
+    {
+        public static void IslandCount_()
+        {
+            Console.WriteLine(IslandCount());
+        }
+        public static int IslandCount()
+        {
+            Console.WriteLine("Hello World!");
+            return 0;
+        }
+
+        
+    }
+}
---
+++ ds_algo/C#/pramp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace pramp
+{
+    public partial class Program
+    {
+        public static void Main(string[] args)
+        {
+            IslandCount_();
+            Console.WriteLine("Test");
+        }
+    }
+}
