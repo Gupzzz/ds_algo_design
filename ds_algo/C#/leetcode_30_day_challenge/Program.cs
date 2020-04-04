6499a85ffe8dab598f3ef4a083ae32a9760d1567
Arun Guptha <arunguptha@Aruns-MBP-15-inch.local>
2020-04-04 17:47:46 +0530

    leetcodechallenge project

---
+++ ds_algo/C#/leetcode_30_day_challenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace leetcode_30_day_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
