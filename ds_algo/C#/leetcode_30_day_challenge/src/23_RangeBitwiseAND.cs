274ec68b3b811ff55ebcd880ffbb8c1740c7b30c
Arun Guptha <arunguptha@MININT-9ROSV4Q.europe.corp.microsoft.com>
2020-05-01 12:29:40 +0530

    bitwise

--- ds_algo/C#/leetcode_30_day_challenge/src/23_RangeBitwiseAND.cs
+++ ds_algo/C#/leetcode_30_day_challenge/src/23_RangeBitwiseAND.cs
@@ -12,6 +12,19 @@ namespace leetcode_30_day_challenge
         when m != n, There is at least an odd number and an even number, so the last bit position result is 0.
         Move m and n rigth a position.
         Keep doing step 1,2,3 until m equal to n, use a factor to record the iteration time.
+
+        The hardest part of this problem is to find the regular pattern.
+        For example, for number 26 to 30
+        Their binary form are:
+        11010
+        11011
+        11100　　
+        11101　　
+        11110
+
+        Because we are trying to find bitwise AND, so if any bit there are at least one 0 and one 1,
+         it always 0. In this case, it is 11000.
+        So we are go to cut all these bit that they are different. In this case we cut the right 3 bit.
         */
         public static int RangeBitwiseAND(int m, int n)
         {
@@ -22,15 +35,21 @@ namespace leetcode_30_day_challenge
             int moveFactor = 1;
             while (m != n)
             {
+                Console.Write(m + "\t");
                 m >>= 1;
+                Console.Write(m + "\t");
+                Console.Write(n + "\t");
                 n >>= 1;
+                Console.Write(n + "\n");
+
                 moveFactor <<= 1;
             }
             return m * moveFactor;
         }
-        public static void _RangeBitwiseAND(){
-            int m = 5, n = 7;
-            Console.WriteLine("Range bit wise AND on " + m + " " + n + " is " +  RangeBitwiseAND(m, n));
+        public static void _RangeBitwiseAND()
+        {
+            int m = 26, n = 30;
+            Console.WriteLine("Range bit wise AND on " + m + " " + n + " is " + RangeBitwiseAND(m, n));
         }
     }
 }
